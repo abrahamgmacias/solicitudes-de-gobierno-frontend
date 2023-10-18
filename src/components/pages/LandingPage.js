7827eca800ae45715a5661bdd22a218bbbd3ddc7
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-18 17:57:44 -0600

    creates simple landing page

---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DashboardEmployee from "../UI/organisms/DashboardEmployee"
+
+export default function LandingPage() {
+    return (
+        <div>
+            <DashboardEmployee></DashboardEmployee>
+        </div>
+    )
+}
