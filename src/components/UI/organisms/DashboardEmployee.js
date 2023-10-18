a30fd3f65dc93bb0bf8090c7bf5bed6061eb7cb4
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-18 17:56:56 -0600

    creates basic dashboard for employee

---
+++ src/components/UI/organisms/DashboardEmployee.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function DashboardEmployee() {
+    return (
+        <div>
+            <nav className="bg-green-500 p-4">
+                <div className="container mx-auto">
+                    <p className="text-white text-xl font-medium font-helvetica">
+                        Gestor de Solicitudes MX
+                    </p>
+                </div>
+            </nav>
+        </div>
+    )
+}
