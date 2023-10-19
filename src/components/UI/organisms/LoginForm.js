d21f94a026e7f27ebc1001681f075fa118c6fff7
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-19 16:35:15 -0600

    adds button styling

--- src/components/UI/organisms/LoginForm.js
+++ src/components/UI/organisms/LoginForm.js
@@ -40,7 +40,10 @@ export default function LoginForm(props) {
             </div>
 
             <div className="flex items-center justify-between">
-                <Button button="Sign in" onClick={null} />
+                <Button button="Sign in"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    onClick={null}
+                />
             </div>
         </form>
     )
