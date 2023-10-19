70b27ef6058355776c237cb0c823f6c27a722a99
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-18 20:13:34 -0600

    updates button to have default design

--- src/components/UI/atoms/Button.js
+++ src/components/UI/atoms/Button.js
@@ -1,5 +1,8 @@
 export default function Button(props) {
     return (
-        <button type="button" className={props.className} onClick={props.onClick}>{props.onClick}</button>
+        <button
+            type="button"
+            className={props.className ? props.className : "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"}
+            onClick={props.onClick}>{props.button}</button>
     )
 }
