d7fa101d9cea1fcff420a0649b802c88c2d32502
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-19 13:48:16 -0600

    factors custom label and inputfield

--- src/components/UI/organisms/LoginForm.js
+++ src/components/UI/organisms/LoginForm.js
@@ -1,15 +1,23 @@
 import Button from '../atoms/Button';
+import InputField from '../atoms/InputField';
+import Label from '../atoms/Label';
 
 export default function LoginForm(props) {
+    const inputStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    const labelStyle = "block text-gray-700 text-sm font-bold mb-2"
+
     return (
         <form>
             <h2 className="text-2xl font-semibold mb-4">Login</h2>
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Username:
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                <Label
+                    className={labelStyle}
+                    for="username"
+                    label="Username:"
+                />
+
+                <InputField
+                    className={inputStyle}
                     id="username"
                     type="text"
                     placeholder="Enter your username"
@@ -17,11 +25,14 @@ export default function LoginForm(props) {
             </div>
 
             <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                    Password:
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                <Label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    for="password"
+                    label="Password:"
+                />
+
+                <InputField
+                    className={inputStyle}
                     id="password"
                     type="password"
                     placeholder="Enter your password"
