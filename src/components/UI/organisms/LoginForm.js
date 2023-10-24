082badd9a40d53a22aa5ed589013b40cf75d3d07
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-24 12:49:33 -0600

    adds loginButtonCLick logic

--- src/components/UI/organisms/LoginForm.js
+++ src/components/UI/organisms/LoginForm.js
@@ -1,26 +1,52 @@
 import Button from '../atoms/Button';
 import InputField from '../atoms/InputField';
 import Label from '../atoms/Label';
+import { useState } from 'react';
+import { loginUser } from '../../../requests/apiCalls';
+import { loginUserCredentials } from '../../../requests/credentials';
+import { selectToken } from '../../../features/Login/tokenSlice';
+import { useNavigate } from 'react-router-dom';
+import { store } from '../../../app/store';
 
 export default function LoginForm(props) {
+    const navigate = useNavigate();
     const inputStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
     const labelStyle = "block text-gray-700 text-sm font-bold mb-2"
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginButtonClick = () => {
+        loginUser(loginUserCredentials(email, password));
+        // console.log(store.getState());
+        // if (selectToken()) {
+        //     navigate('/');
+        // }
+    }
+
+    const formPreventDefault = (e) => {
+        e.preventDefault();
+        if (e.key === "Enter") {
+            loginButtonClick();
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={formPreventDefault} onKeyUp={formPreventDefault}>
             <h2 className="text-2xl font-semibold mb-4">Login</h2>
             <div className="mb-4">
                 <Label
                     className={labelStyle}
-                    for="username"
-                    label="Username:"
+                    for="email"
+                    label="Email:"
                 />
 
                 <InputField
                     className={inputStyle}
-                    id="username"
+                    id="email"
                     type="text"
-                    placeholder="Enter your username"
+                    placeholder="Enter your email:"
+                    setState={setEmail}
                 />
             </div>
 
@@ -36,13 +62,14 @@ export default function LoginForm(props) {
                     id="password"
                     type="password"
                     placeholder="Enter your password"
+                    setState={setPassword}
                 />
             </div>
 
             <div className="flex items-center justify-between">
                 <Button button="Sign in"
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    onClick={null}
+                    onClick={loginButtonClick}
                 />
             </div>
         </form>
