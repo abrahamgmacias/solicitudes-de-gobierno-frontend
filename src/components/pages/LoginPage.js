6c15bb8680cd63dd59e45520758cb9fe8978ccda
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-25 15:10:13 -0600

    adds navbar

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,13 +1,20 @@
 import LoginForm from '../UI/organisms/LoginForm';
+import Navbar from '../UI/molecules/Navbar';
 import Card from '../UI/atoms/Card';
+
 
 export default function LoginPage(props) {
     return (
-        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-            <Card className="bg-white p-8 rounded shadow-md w-80" render={() => (
-                <LoginForm></LoginForm>
-            )}>
-            </Card>
+        // factor last div
+        <div>
+            <Navbar></Navbar>
+            <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+                <Card className="bg-white p-8 rounded shadow-md w-80" render={() => (
+                    <LoginForm></LoginForm>
+                )}>
+                </Card>
+            </div>
         </div>
+
     );
 };
