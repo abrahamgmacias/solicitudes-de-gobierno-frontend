29d1144be519e7037b6432e29ae2a8c28143c1d1
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-25 15:09:52 -0600

    adds loginButtons props

--- src/components/UI/molecules/Navbar.js
+++ src/components/UI/molecules/Navbar.js
@@ -6,28 +6,29 @@ import { Link } from 'react-router-dom';
 export default function Navbar(props) {
     const buttonStyle = "bg-white text-green-500 hover:bg-green-100 hover:text-green-600 font-medium py-2 px-4 rounded"
 
-    // check if user is logged in
-
     return (
         <nav className="bg-green-500 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <p className="text-white text-xl font-medium font-helvetica">
                     Gestor de Solicitudes MX
                 </p>
-                <div className="flex space-x-4">
-                    <Link to="/login">
+
+                {props.loginButtons ?
+                    <div className="flex space-x-4">
+                        <Link to="/login">
+                            <Button
+                                className={buttonStyle}
+                                button="Log In"
+                                onClick={null}
+                            />
+                        </Link>
+
                         <Button
                             className={buttonStyle}
-                            button="Log In"
-                            onClick={null}
+                            button="Register"
                         />
-                    </Link>
+                    </div> : null}
 
-                    <Button
-                        className={buttonStyle}
-                        button="Register"
-                    />
-                </div>
             </div>
         </nav>
     )
