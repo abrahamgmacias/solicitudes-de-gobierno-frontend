7836b69f8eff5f4ab1872e19988e82783ded57cb
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-26 11:00:22 -0600

    adds styling

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -8,8 +8,8 @@ export default function LoginPage(props) {
         // factor last div
         <div>
             <Navbar></Navbar>
-            <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-                <Card className="bg-white p-8 rounded shadow-md w-80" render={() => (
+            <div className="bg-gray-100 min-h-screen flex items-start justify-center">
+                <Card className="bg-white p-8 rounded shadow-md w-80 mt-5" render={() => (
                     <LoginForm></LoginForm>
                 )}>
                 </Card>
