c6d6032b95ef945ebb9dad0d4dfa995831860da3
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-26 11:02:10 -0600

    adds tokenAvailable logic

--- src/components/UI/organisms/DashboardEmployee.js
+++ src/components/UI/organisms/DashboardEmployee.js
@@ -1,10 +1,13 @@
 import Navbar from '../molecules/Navbar'
-
+import { useSelector } from 'react-redux';
+import { selectTokenValue } from '../../../features/Login/tokenSlice';
 
 export default function DashboardEmployee() {
+    const isTokenAvailable = useSelector(selectTokenValue);
+
     return (
         <div>
-            <Navbar loginButtons={true} />
+            <Navbar loginButtons={isTokenAvailable ? false : true} />
             <p>Welcome employee</p>
         </div>
     )
