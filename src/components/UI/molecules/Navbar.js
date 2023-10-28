2963c1c8f77240d3413cfadd88fb8b034f91d037
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-27 18:05:50 -0600

    changes styling

--- src/components/UI/molecules/Navbar.js
+++ src/components/UI/molecules/Navbar.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { clearToken, selectTokenValue } from '../../../features/Login/tokenSlice';
 
 export default function Navbar(props) {
-    const buttonStyle = "bg-white text-green-500 hover:bg-green-100 hover:text-green-600 font-medium py-2 px-4 rounded"
+    const buttonStyle = "bg-white text-green-500 hover:bg-green-100 hover:text-green-600 font-medium py-2 px-4 rounded items-center"
     const isTokenAvailable = useSelector(selectTokenValue);
 
     function logOut() {
@@ -15,16 +15,16 @@ export default function Navbar(props) {
     };
 
     return (
-        <nav className="bg-green-500 p-4">
+        <nav className="bg-green-500 h-16 p-4 items-center">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to={"/"}>
-                    <p className="text-white text-xl font-medium font-helvetica">
+                    <p className="text-white text-xl font-helvetica font-bold m-0 p-0">
                         Gestor de Solicitudes MX
                     </p>
                 </Link>
 
                 {props.loginButtons ?
-                    <div className="flex space-x-4">
+                    <div className="flex space-x-4 items-center">
                         <Link to="/login">
                             <Button
                                 className={buttonStyle}
@@ -41,7 +41,7 @@ export default function Navbar(props) {
                 }
 
                 {props.logoutButtons ?
-                    <div className="flex space-x-4">
+                    <div className="flex space-x-4 items-center">
                         <Link to="/">
                             <Button
                                 className={buttonStyle}
