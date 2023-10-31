91966a314f006aba3aba75726bd0697763c94fad
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-10-31 15:49:02 -0600

    adds create button

--- src/components/UI/molecules/DashboardButtonLayout.js
+++ src/components/UI/molecules/DashboardButtonLayout.js
@@ -3,7 +3,7 @@ import Button from '../atoms/Button';
 // Add role logic
 export default function DashboardButtonLayout() {
     const buttonStyle = "bg-gray-400 border border-black px-6 py-2 hover:bg-gray-500 hover:text-white transition duration-300 ml-4 mt-4";
-    const optionList = ['Mis solicitudes', 'Solicitudes cerca de mi']
+    const optionList = ['Mis solicitudes', 'Solicitudes cerca de mi', 'Crear solicitudes']
 
     const buttons = optionList.map((text, index) => (
         <Button className={buttonStyle} key={index} button={text} />
