60c7aa8b73682a62120becfd8ae3d8bc98765cfe
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-17 16:27:14 -0600

    adds leftArrow

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,18 +1,24 @@
 import LoginForm from '../UI/organisms/LoginForm';
+import LeftArrow from '../UI/atoms/LeftArrow';
 import Card from '../UI/atoms/Card';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage(props) {
-    return (
-        // factor last div
-        <div>
-            <div className="bg-gray-100 min-h-screen flex items-start justify-center">
-                <Card className="bg-white p-8 rounded shadow-md w-80 mt-5" render={() => (
-                    <LoginForm></LoginForm>
-                )}>
-                </Card>
-            </div>
-        </div>
+    const navigate = useNavigate();
+    
+    const handleClick = () => {
+        navigate(-1);
+    }
 
+    return (
+        <div className="bg-gray-100 min-h-screen flex items-start justify-center">
+            <br></br>
+            <Card className="bg-white p-8 rounded shadow-md w-80 mt-5" render={() => (
+                <div>
+                    <LeftArrow className="cursor-pointer flex justify-end" onClick={handleClick} text="Go back &larr;"/>
+                    <LoginForm></LoginForm>
+                </div>
+            )}/>
+        </div>
     );
 };
