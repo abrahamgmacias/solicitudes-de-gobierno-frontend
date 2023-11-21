9fcf11702a0eb4ef562440ac1c9656b5784dc1a0
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-11-21 01:11:12 -0600

    modifies getLibro

--- src/requests/apiCalls.js
+++ src/requests/apiCalls.js
@@ -1,4 +1,3 @@
-import { Warning } from "postcss";
 import { store } from '../app/store';
 import { url } from "./url";
 import { setRole } from "../features/Login/roleSlice";
@@ -6,23 +5,48 @@ import { setToken } from "../features/Login/tokenSlice";
 import { setFirstName, setLastName } from "../features/User/userSlice";
 
 export async function loginUser(credentials) {
-    return fetch(`${url}/login`, credentials)
+    return fetch(`${url}/auth/login`, credentials)
         .then((res) => {
-            if (res.status === 401) {
-                new Warning("Email or password is incorrect.")
+            if (res.status === 404 || res.status === 401) {
+                throw new Error("We had a problem logging you in. Try again.");
             }
 
             return res.json();
         })
         .then((json) => {
-            const { first_name, last_name, token, role } = json;
+            const { nombre, apellido_paterno, token } = json;
 
             store.dispatch(setToken(token));
-            store.dispatch(setFirstName(first_name));
-            store.dispatch(setLastName(last_name));
-            store.dispatch(setRole(role));
+            store.dispatch(setFirstName(nombre));
+            store.dispatch(setLastName(apellido_paterno));
+            // store.dispatch(setRole(role));
+
+            return { success: true, message: "You'll be redirected shortly..." }
+            
         })
         .catch((err) => {
-            console.log(err)
+            return { success: false, message: err.message}
+        });
+}
+
+
+export async function getLibro(credentials, libro_id=null) {
+    // Allow to search by id
+    let baseUrl = `${url}/libros`
+    if (libro_id) {
+        baseUrl += `/${libro_id}`
+    }
+
+    return fetch(`${baseUrl}`, credentials)
+        .then((res) => {
+            if (res.status === 500 || res.status === 401) {
+                throw new Error("We had a problem logging you in. Try again.");
+            }
+
+            return res.json();
+        })
+        .catch((err) => {
+            console.log(err);
+            return err;
         });
 }
